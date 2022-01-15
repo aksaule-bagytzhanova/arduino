94e5e7ce753cf575197129c1b2450f41386e1741
Aksi <aksaule124026@gmail.com>
2022-01-15 18:40:00 +0600

    Third lesson video

---
+++ third_lesson/third_lesson.ino
@@ -0,0 +1,10 @@
+int led =3;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  analogWrite(led,10);
+}
