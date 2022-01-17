19bfa11ff3aebf96111fb66c916b5942a4b5c5c2
Aksi <aksaule124026@gmail.com>
2022-01-18 02:06:15 +0600

    Fifth lesson

---
+++ fifth_lesson/fifth_lesson.ino
@@ -0,0 +1,12 @@
+int pot = 0;
+int val = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = analogRead(pot);
+  Serial.println(val);
+}
