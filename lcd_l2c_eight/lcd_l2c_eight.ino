14232c6fffcac3f1d893850b0f5f3bc6ed5e07a5
Aksi <aksaule124026@gmail.com>
2022-01-25 02:17:11 +0600

    lesson

---
+++ lcd_l2c_eight/lcd_l2c_eight.ino
@@ -0,0 +1,60 @@
+//#include <Wire.h>
+//#include <LiquidCrystal_I2C.h>
+//
+//LiquidCrystal_I2C lcd(0x76, 16, 2);
+//void setup() {
+//  // put your setup code here, to run once:
+//  lcd.init();
+//  lcd.backlight();
+//}
+//
+//void loop() {
+//  // put your main code here, to run repeatedly:
+//  lcd.setCursor(0,0);
+//  lcd.print("BeyondRobotics");
+//  lcd.setCursor(0,1);
+//  lcd.print("Arduino");
+//  
+//}
+
+#include "Wire.h"
+ 
+void setup(){
+    Wire.begin();    
+    Serial.begin(9600);
+} 
+ 
+void loop(){
+    byte error, address;
+    int nDevices;
+ 
+    Serial.println("Scanning...");
+ 
+    nDevices = 0;
+    for(address = 8; address < 127; address++ ){
+        Wire.beginTransmission(address);
+        error = Wire.endTransmission();
+ 
+        if (error == 0){
+            Serial.print("I2C device found at address 0x");
+            if (address < 16)
+                Serial.print("0");
+            Serial.print(address,HEX);
+            Serial.println(" !");
+ 
+            nDevices++;
+        }
+        else if (error == 4) {
+            Serial.print("Unknow error at address 0x");
+            if (address < 16)
+                Serial.print("0");
+            Serial.println(address,HEX);
+        } 
+    }
+    if (nDevices == 0)
+        Serial.println("No I2C devices found\n");
+    else
+        Serial.println("done\n");
+ 
+    delay(5000);
+}
